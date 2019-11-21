4baf645da0394e566274ec79927430bc95cb4872
alfar <alfarocubero.enrique5@gmail.com>
2019-11-20 19:03:13 -0600

    cambios en el conection de familias

--- familias/db/connection/conexion.php
+++ familias/db/connection/conexion.php
@@ -1,5 +1,14 @@
 <?php
-$mysqli = new mysqli("localhost", "ementor1_familia", "3men7oreS%", "ementor1_familias");
+// $mysqli = new mysqli("localhost", "ementor1_familia", "3men7oreS%", "ementor1_familias");
+// if ($mysqli->connect_errno) {
+//     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+// }
+// $mysqli->set_charset("utf8");
+?>
+
+
+<?php
+$mysqli = new mysqli("localhost", "root", "", "familias");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
