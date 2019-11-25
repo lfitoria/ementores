42e155d35d54d09bafe2068693f4da0cdfa9290d
lfitoria <lfitoria@eldomo.net>
2019-11-25 13:28:19 -0700

    cambios en el server ulitmos

--- familias/mod_3/3-05_vid.php
+++ familias/mod_3/3-05_vid.php
@@ -408,9 +408,9 @@
 
             $("#video2").on("timeupdate", function() {
                 console.log(parseInt(this.currentTime * 1000));
-                if (parseInt(this.currentTime * 1000) >= 33100 && parseInt(this.currentTime * 1000) <= 33200) {
+                if (parseInt(this.currentTime * 1000) >= 33500 && parseInt(this.currentTime * 1000) <= 33900) {
                     this.pause();
-                    time = 24;
+                    time = 34;
                     video = document.getElementById("video2");
                     modal = $(this).parent().parent().find(".modal")
                     modal.modal("show");
