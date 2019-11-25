77916cb2c693dddc5709e468370d45535f58557b
lfitoria <lfitoria@eldomo.net>
2019-11-25 09:10:58 -0700

    Kit-folfer-final

--- familias/db/connection/conexion.php
+++ familias/db/connection/conexion.php
@@ -1,14 +1,5 @@
 <?php
-// $mysqli = new mysqli("localhost", "ementor1_familia", "3men7oreS%", "ementor1_familias");
-// if ($mysqli->connect_errno) {
-//     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-// }
-// $mysqli->set_charset("utf8");
-?>
-
-
-<?php
-$mysqli = new mysqli("localhost", "root", "", "familias");
+$mysqli = new mysqli("localhost", "ementor1_familia", "3men7oreS%", "ementor1_familias");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
