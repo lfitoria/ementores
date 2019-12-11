3ed0110a255549c4b8a9d6dd80b21f3183fae80b
lfitoria <lfitoria@eldomo.net>
2019-12-11 13:27:30 -0600

    htacces-synfonyUpdate

--- familias/db/connection/conexion.php
+++ familias/db/connection/conexion.php
@@ -1,5 +1,9 @@
 <?php
-$mysqli = new mysqli("localhost", "ementor1_familia", "3men7oreS%", "ementor1_familias");
+// $mysqli = new mysqli("localhost", "ementor1_familia", "3men7oreS%", "ementor1_familias");
+// if ($mysqli->connect_errno) {
+//     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+// }
+$mysqli = new mysqli("localhost", "root", "root", "ementores_familias");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
