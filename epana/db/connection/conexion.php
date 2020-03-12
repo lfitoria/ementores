c443c5e58c5890793c25c0fad6925cc81d71e6d7
lfitoria <lfitoria@eldomo.net>
2020-03-12 09:45:19 -0600

    cortepull

--- epana/db/connection/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "root", "rp_ementores_jovenes");
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
-$mysqli->set_charset("utf8");
-
-?>
