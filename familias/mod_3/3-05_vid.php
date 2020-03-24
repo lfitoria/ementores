ff398ce565ade8bc117b5100c9e2759395e298c7
lfitoria <lfitoria@eldomo.net>
2020-03-24 16:53:18 -0600

    cambioscorreo24-3-20-final

--- familias/mod_3/3-05_vid.php
+++ familias/mod_3/3-05_vid.php
@@ -301,7 +301,7 @@
                 <div class="container-full footer-bg-1 padding-top-bottom-footer"></div>
                 <div class="container-full footer-bg-2 padding-top-bottom-footer"></div>
                 <div class="container-full footer-bg-3 footer-div-copyright">
-                    <p class="text-white text-center no-margin-top-bottom text-opensans">Todos los derechos reservados &copy; Paniamor 2018</p>
+                    <p class="text-white text-center no-margin-top-bottom text-opensans">Todos los derechos reservados &copy; Ministerio de Educación - República Dominicana <?= date("Y") ?></p>
                 </div>
             </footer>
         </div>
