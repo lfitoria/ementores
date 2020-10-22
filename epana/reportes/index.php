175fcc832e0fdbc77d510702c399ee62e5e12edd
lfitoria <lfitoria@eldomo.net>
2020-10-22 10:21:37 -0600

    cambios

--- epana/reportes/index.php
+++ epana/reportes/index.php
@@ -2,6 +2,6 @@
 <?php
 /* This will give an error. Note the output
  * above, which is before the header() call */
-header('Location: /db/user/get_users.php');
+header('Location: http://rd.ementores.org/epana/db/user/get_users.php');
 exit;
 ?>
